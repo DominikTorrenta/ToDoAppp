595c4da61dba49d2830538ec1f6cef5960239936
jorik0508 <79000572+jorik0508@users.noreply.github.com>
2022-05-12 17:30:07 +0300

    Revert "Screen 1 full completed"

--- app/src/main/java/com/example/todoappp/MainActivity.kt
+++ app/src/main/java/com/example/todoappp/MainActivity.kt
@@ -2,32 +2,11 @@ package com.example.todoappp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.todoappp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        val validator = Validator()
-
-        binding.button.setOnClickListener{
-
-            binding.InputEmail.error =
-            validator.emailvalid(binding.emailText.text)
-            binding.InputPassword.error =
-            validator.passwordvalid(binding.passwordText.text)
-
-                if (binding.InputEmail.error == null && binding.InputPassword.error == null)
-                toastShow(string = getString(R.string.success))
-        }
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
-
-    fun toastShow(string: String) =
-        Toast.makeText(applicationContext, string, Toast.LENGTH_SHORT).show()
-
 }
